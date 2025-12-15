fc484338a1acd8b70aa6c1ebb06f569657ca5f3a
Brandi Bateman <takenbrandi@gmail.com>
2025-12-14 19:19:02 -0800

    Day 1-Part 2

--- solutions/day-1.hs
+++ solutions/day-1.hs
@@ -4,15 +4,19 @@ import           Control.Applicative              (Alternative ((<|>)))
 import qualified Data.Attoparsec.ByteString.Char8 as A
 import qualified Data.ByteString                  as B
 import           Data.Either                      (fromRight)
-import           GHC.List                         (foldl')
+import           Data.List                        (iterate')
 import           System.Environment               (getArgs)
-
 
 type Input = [Step]
 type Solution = Int
 data Step = Step Direction Int deriving (Show)
-data Direction = L | R deriving (Show)
+data Direction = L | R deriving (Eq, Show)
 newtype Dial = Dial Int deriving (Eq, Ord, Show)
+data StepState = SS {
+  _dial     :: Dial,
+  positions :: [Int]
+}
+type StepHandler = Step -> Dial -> StepState
 
 mkDial :: Int -> Dial
 mkDial n
@@ -20,29 +24,49 @@ mkDial n
   | n > 99 = mkDial (n - 100)
   | otherwise = Dial n
 
-moveDial :: Dial -> Step -> Dial
-moveDial (Dial pos) (Step direction distance) =
+unwrap :: Dial -> Int
+unwrap (Dial n) = n
+
+op :: Direction -> Int -> Int -> Int
+op = \case
+  L -> (-)
+  R -> (+)
+
+-- | Moves dial and reports end position into state
+moveDial :: Step -> Dial -> StepState
+moveDial (Step dir dist) (Dial pos) =
+  let dial' = mkDial $ op dir pos dist
+  in SS dial' [unwrap dial']
+
+-- | Moves dial and reports all passed positions into state
+moveDial' :: Step -> Dial -> StepState
+moveDial' (Step dir dist) dial =
   let
-    op = case direction of
-      L -> (-)
-      R -> (+)
-    in mkDial (pos `op` distance)
+    SS dial' _ = moveDial (Step dir dist) dial
+    dials = take dist $ tail $ iterate' (mkDial . flip (op dir) 1 . unwrap) dial
+  in SS dial' (unwrap <$> dials)
 
 parser :: B.ByteString -> Input
 parser = fromRight [] . A.parseOnly (A.sepBy stepP (A.char '\n'))
   where
     stepP = Step <$> directionP <*> A.decimal
-    directionP = L <$ A.char 'L' <|> R <$ A.char 'R'
+    directionP =
+          L <$ A.char 'L'
+      <|> R <$ A.char 'R'
 
 solve1 :: Input -> Solution
-solve1 = snd . foldl' dostep (mkDial 50, 0)
-  where
-    dostep (dial, count) step =
-      let dial'@(Dial pos') = moveDial dial step
-      in (dial', if pos' == 0 then count + 1 else count)
+solve1 = solve moveDial
 
 solve2 :: Input -> Solution
-solve2 = error "Part 2 Not implemented"
+solve2 = solve moveDial'
+
+solve :: StepHandler -> Input -> Solution
+solve handler = getCount . foldl doStep (SS (mkDial 50) [])
+  where
+    doStep (SS dial orig) step =
+      let SS dial' new = handler step dial
+      in SS dial' (orig ++ new)
+    getCount = length . filter (== 0) . positions
 
 main :: IO ()
 main = do
@@ -58,4 +82,3 @@ main = do
     else do
       putStrLn "solution to problem 2 is:"
       print $ solve2 input
-
